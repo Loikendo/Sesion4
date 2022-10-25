97551c9bce709f6244f51d711976ea6c80e67478
Alberto <Alberto@MSI>
2022-10-25 10:18:49 +0200

    Version 1

---
+++ Sesion4/src/test/TestCuenta.java
@@ -0,0 +1,42 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TestCuenta {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+	
+	public void testIngreso() {
+		assertEquals(100, Cuenta.ingresar(100));
+	}
+	
+	public void testRetirada() {
+		assertEquals(0, Cuenta.retirar(100));
+	}
+
+}
